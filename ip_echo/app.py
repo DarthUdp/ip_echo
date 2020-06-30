d7d563b46f35e60e4f5ef6e0ed313eab860cd2e5
Matheus Xavier <mxavier@mail.cmdforge.com>
2020-06-30 02:29:58 -0300

    ready for release

---
+++ ip_echo/app.py
@@ -0,0 +1,63 @@
+import json
+from http import HTTPStatus
+
+from flask import Flask, request, Response, render_template
+from marko.ext.gfm import gfm
+from uuid import uuid4
+from yaml import Dumper, dump as ydump
+import sqlite3
+
+from markupsafe import escape
+
+app = Flask(__name__)
+
+def read_file(path):
+    with open(path) as fp:
+        return fp.read()
+
+
+@app.route('/')
+def hello_world():
+    motdl = []
+    conn = sqlite3.connect("ip_echo/motd.db")
+    c = conn.cursor()
+    for msg in c.execute("""SELECT * FROM `motd` WHERE `motd`.`valid` = 1"""):
+        motdl.append(gfm(msg[1]))
+    client_ip = request.remote_addr
+    md = render_template("index.md", client_ip=client_ip)
+    rendered_md = gfm(md)
+    html = render_template("index.html", markdown_rendered=rendered_md,
+                           request_id=uuid4().hex, motdl=motdl)
+    return Response(content_type="text/html", response=html)
+
+
+@app.route('/address.<string:ext>')
+def return_rendered(ext):
+    ext = escape(ext)
+    rid = uuid4().hex
+    headers = {
+        "x-request-id": rid
+    }
+    res = {
+        "ip": request.remote_addr,
+        "request_id": rid
+    }
+    if ext == "txt":
+        if request.args.get("plain", None) is not None:
+            resp = "{ip},{request_id}".format(**res)
+            return Response(content_type="text/plain", response=resp, headers=headers)
+        else:
+            resp = "ip={ip}\nrequest_id={request_id}".format(**res)
+            return Response(content_type="text/plain", response=resp, headers=headers)
+    elif ext == "json":
+        return Response(content_type="application/json", response=json.dumps(res), headers=headers)
+    elif ext == "yaml" or ext == "yml":
+        return Response(content_type="application/x-yaml", response=ydump(res, Dumper=Dumper), headers=headers)
+    else:
+        return Response(status=HTTPStatus.NOT_FOUND,
+                        response=gfm(render_template("404.md", url=request.url, request_id=rid))
+                        )
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
---
+++ ip_echo/templates/index.md
@@ -0,0 +1,13 @@
+Your ip is **{{client_ip}}**
+
+_**THIS SERVICE IS OFFERED IN AN AS-IS BASIS WITH NO GUARANTEES REGARDING ITS UPTIME,
+I RESERVE MYSELF THE RIGHT TO TAKE THIS SITE OFFLINE AT ANYTIME I SEE FIT, WITHOUT ANY OBLIGATION TO JUSTIFY IT**_
+
+**The urls are static and are not subject to change**
+
+To get the ip in a more computer friendly format call `/address.<format>` where format is one of the formats bellow:
+* [.txt](address.txt) plain text key pairs, additionally pass call [?plain](address.txt?plain) and only the ip in plain text will be returned
+* [.json](address.json) json format provides a bit more info
+* [.yaml](address.yaml)/yml same as json but yaml formatted
+
+This service is open source the code is in my [github](https://github.com/CardinalBytes/daemonize-me).
